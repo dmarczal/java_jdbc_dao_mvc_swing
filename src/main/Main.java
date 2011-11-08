03f84f43cca5afe7163871b7b53e1260e6db8c54
Diego Marczal <dmarczal@gmail.com>
2011-11-08 21:10:21 -0200

    Refactore database and clean view

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,23 +3,13 @@ package main;
 import java.sql.SQLException;
 
 import model.User;
-import view.MainJFrame;
 
 public class Main {
 	
 	public static void main(String[] args) throws SQLException {
-		//textMode();
-
-		//Schedule a job for the event-dispatching thread:
-        //creating and showing this application's GUI.
-        javax.swing.SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                MainJFrame.createAndShowGUI();
-            }
-        });
+		textMode();
 	}		
-	
-	@SuppressWarnings("unused")
+
 	private static void textMode() throws SQLException{
 		System.out.println("Apagando todos os usu‡rios do Banco de Dados");
 		System.out.println(User.deleteAll() + " usu‡rios deletados");
